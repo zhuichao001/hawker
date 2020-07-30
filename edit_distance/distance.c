68eb0988c486e71eed4cc5cc818ed711ee5fc632
qiuzerzhang <qiuzerzhang@tencent.com>
2020-07-30 16:28:03 +0800

    改进代码

--- edit_distance/distance.c
+++ edit_distance/distance.c
@@ -4,22 +4,14 @@
 
 using namespace std;
 
-int min3(int a, int b, int c){
-    if (a<b){
-        return a<c?a:(b<c?b:c);
-    } else {
-        return b<c?b:c;
-    }
+int min(int a, int b){
+    return a<b?a:b;
 }
 
 int minDist(string a, string b){
     const int M=a.size(), N=b.size();
-    int dp[M+1][N+1];
+    int dp[2][N+1];
     memset(dp, 0, sizeof(dp));
-
-    for(int i=1; i<=M; i++){
-        dp[i][0]=i;
-    }
 
     for(int j=1; j<=N; j++){
         dp[0][j]=j;
@@ -27,14 +19,15 @@ int minDist(string a, string b){
 
     for(int i=0; i<M; i++){
         for(int j=0; j<N; j++){
+            dp[i&1][0] = i;
             if(a[i]==b[j]){
-                dp[i+1][j+1] = dp[i][j];
+                dp[(i+1)&1][j+1] = dp[i&1][j];
             }else{
-                dp[i+1][j+1] = 1+min3(dp[i][j], dp[i][j+1], dp[i+1][j]);
+                dp[(i+1)&1][j+1] = 1+min(dp[i&1][j], dp[i&1][j+1]);
             }
         }
     }
-    return dp[M][N];
+    return dp[M&1][N];
 }
 
 int main(){
