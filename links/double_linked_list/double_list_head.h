9e3039322bea15cdd84af6045515dc5edc70ac93
qiuzerzhang <qiuzerzhang@tencent.com>
2020-08-17 12:09:45 +0800

    内置双链表

---
+++ links/double_linked_list/double_list_head.h
@@ -0,0 +1,81 @@
+﻿#ifndef _LinkedListNode_H_
+#define _LinkedListNode_H_
+
+
+typedef struct ListNode {
+    ListNode *next;
+    ListNode *prev;
+}ListNode;
+
+typedef ListNode ListHead;
+
+
+//add node between prev and next
+inline void __list_add(ListNode *node, ListNode *prev, ListNode *next) {
+    next->prev = node;
+    node->next = next;
+    node->prev = prev;
+    prev->next = node;
+}
+
+
+inline void __list_del(ListNode * prev, ListNode * next) {
+    next->prev = prev;
+    prev->next = next;
+}
+
+
+inline void init_list_head(ListNode *head) {
+    head->next = head;
+    head->prev = head;
+}
+
+
+inline void list_add(ListNode *node, ListNode *head) {
+    __list_add(node, head, head->next);
+}
+
+
+inline void list_add_tail(ListNode *node, ListNode *head) {
+    __list_add(node, head->prev, head);
+}
+
+
+inline void list_del(ListNode *entry) {
+    __list_del(entry->prev, entry->next);
+    entry->prev = 0;
+    entry->next = 0;
+}
+
+
+inline void list_replace(ListNode *old, ListNode *young) {
+    young->next = old->next;
+    young->next->prev = young;
+    young->prev = old->prev;
+    young->prev->next = young;
+}
+
+
+inline int list_empty(const ListNode *head) {
+    return head->next == head;
+}
+
+
+//offset of TYPE.MEMBER
+#define offset_of(type, member) ((size_t) &((type *)0)->member)
+
+//get TYPE's ptr by MEMBER's ptr
+#define container_of(ptr, type, member) ({                \
+    const typeof( ((type *)0)->member ) *__mptr = (ptr);  \
+    (type *)( (char *)__mptr - offset_of(type,member) );})
+
+#define list_entry(ptr, type, member) \
+    container_of(ptr, type, member)
+
+#define list_for_each(cur, head) \
+    for (cur=(head)->next; cur!=(head); cur=cur->next)
+
+#define list_for_each_safe(current, forward, head) \
+    for (current=(head)->next, forward=current->next; current!=(head); current=forward, forward=pos->next)
+
+#endif
---
+++ links/double_linked_list/test_double_list.c
@@ -0,0 +1,74 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <memory.h>
+#include "double_list_head.h"
+
+
+struct Person {
+    int age;
+    char name[20];
+    ListHead list;
+
+    Person() {
+        init_list_head(&list);
+        name[0] = 0;
+    }
+
+    Person(int _age, char *_name) {
+        age = _age;
+        int len = strlen(_name);
+        memcpy(name, _name, len);
+        name[len] = 0;
+        init_list_head(&list);
+    }
+};
+
+
+void printPersonList(Person *person_head) {
+    printf("\n==== for each print ====\n");
+    ListNode *pos;
+    list_for_each(pos, &person_head->list) {
+        Person * entry = list_entry(pos, Person, list);
+        printf("name:%s, age:%d\n", entry->name, entry->age);
+    }
+    printf("\n");
+
+}
+
+
+int main (int argc, char* argv[]) {
+    Person person_head;
+
+    //add Persons
+    for (int i=0; i<5; ++i) {
+        char bufname[20]={0,};
+        sprintf(bufname, "I am no:%d", i+1);
+        Person *p = new Person(18+i, bufname);
+
+        list_add_tail(&p->list, &person_head.list);
+    }
+
+    printPersonList(&person_head);
+
+    //del Node of age:20
+    printf("==== delete node(age:20) ====\n");
+    ListNode *pos, *next;
+    list_for_each_safe(pos, next, &person_head.list) {
+        Person *entry = list_entry(pos, Person, list);
+        if(entry->age == 20) {
+            list_del(pos);
+            free(entry);
+        }
+    }
+
+    printPersonList(&person_head);
+
+    //release all nodes
+    list_for_each_safe(pos, next, &person_head.list) {
+        Person *entry = list_entry(pos, Person, list);
+        list_del(pos);
+        free(entry);
+    }
+    return 0;
+}
