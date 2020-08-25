452c2c718c144f652a6bb903414e25a6c1486bd5
qiuzerzhang <qiuzerzhang@tencent.com>
2020-08-25 16:39:01 +0800

    线段树：抽象函数指针

--- segment_tree/segtree.cpp
+++ segment_tree/segtree.cpp
@@ -9,82 +9,66 @@ inline int sum(int a, int b){
     return a+b;
 }
 
+typedef int (*ReduceFunc)(int, int);
+
 struct SegmentTree{
     int *data, n;
-    int *max_tree;
-    int *sum_tree;
+    int *tree;
+    ReduceFunc func;
 
-    SegmentTree(int *_data, int _n):data(_data), n(_n){
+    SegmentTree(int *_data, int _n, ReduceFunc f):data(_data), n(_n), func(f){
         build();
     }
 
     ~SegmentTree(){
-        delete []max_tree;
-        delete []sum_tree;
+        delete []tree;
     }
 
     void build(){
-        max_tree = new int[n<<1];
-        sum_tree = new int[n<<1];
+        tree = new int[n<<1];
 
         for(int j=0; j<n; ++j){
-            max_tree[n+j] = data[j];
-            sum_tree[n+j] = data[j];
+            tree[n+j] = data[j];
         }
+
         for(int i=n-1; i>0; --i){
-            int left = i*2, right = i*2+1;
-            max_tree[i] = max(max_tree[left], max_tree[right]);
-            sum_tree[i] = sum(sum_tree[left], sum_tree[right]);
+            tree[i] = func(tree[2*i], tree[2*i+1]);
         }
     }
 
     void print(){
         for(int i=1; i<2*n; ++i){
-            cout<<"i="<<i<<", max_val="<<max_tree[i]<<endl;
+            cout<<"i="<<i<<", val="<<tree[i]<<endl;
         }
     }
 
-    int max_range(int i, int j){
+    int query_range(int i, int j){
         int l=i+n, r=j+n;
-        int maxval = 1<<31;
+        int val = 0; //TODO init val
         while(l<=r){
             if(l%2==1){
-                maxval = max(max_tree[l], maxval);
+                val = func(tree[l], val);
                 ++l;
             }
             if(r%2==0){
-                maxval = max(max_tree[r], maxval);
+                val = func(tree[r], val);
                 --r;
             }
             l = l/2;
             r = r/2;
         }
-        return maxval;
-    }
-
-    int sum_range(int i, int j){
-        int l=i+n, r=j+n;
-        int sumval = 0;
-        while(l<=r){
-            if(l%2==1){
-                sumval += sum_tree[l];
-                ++l;
-            }
-            if(r%2==0){
-                sumval += sum_tree[r];
-                --r;
-            }
-            l = l/2;
-            r = r/2;
-        }
-        return sumval;
+        return val;
     }
 };
 
 
 int main(){
     int a[6]={6,4,5,9,7,3};
-    SegmentTree st(a, 6);
-    int s = st.max_range(0,2);
-    cout<<"sum:"<<s<<endl;
+    SegmentTree st(a, 6, max);
+    int s = st.query_range(0,5);
+    cout<<"max result:"<<s<<endl;
+
+    SegmentTree st2(a, 6, sum);
+    int t = st2.query_range(0,5);
+    cout<<"sum result:"<<t<<endl;
 }
