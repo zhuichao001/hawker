ce6be17a10f922e1a0edc134a82379cd498dc8b5
qiuzerzhang <qiuzerzhang@tencent.com>
2020-09-17 16:08:53 +0800

    提交查找某个值大的大素数

---
+++ primer/get_primes.h
@@ -0,0 +1,11 @@
+#include "miller_rabin_primes.h"
+
+
+uint64_t get_prime_gt(uint64_t start){
+    while(true){
+        if(isPrime(start){
+            return start;
+        }
+        start++;
+    }
+}
