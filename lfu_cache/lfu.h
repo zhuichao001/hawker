4a24b16252f6620906e887ab4d01809d88a1d84a
zhuichao001 <zhuichao001@163..com>
2020-09-27 15:04:17 +0800

    fix bug: LFU CACHE

--- lfu_cache/lfu.h
+++ lfu_cache/lfu.h
@@ -74,12 +74,12 @@ struct FreqNode{
     }
 
     void append(Node *node){
+        node->down = nullptr;
         node->slot = this;
         if(first == nullptr){
             first = last = node;
             return;
         }
-
         last->down = node;    
         node->up = last;
         last = node;
@@ -94,7 +94,7 @@ struct LFUCache{
 
     LFUCache(int c):capacity(c),size(0),freq_list(0){}
 
-    void add_freq(FreqNode *br, int freq){
+    void add_slot(FreqNode *br, int freq){
         FreqNode *tmp = new FreqNode(freq);
         if(br->post != nullptr){
             tmp->post = br->post;
@@ -104,9 +104,13 @@ struct LFUCache{
         br->post = tmp;
     }
 
-    void del_freq(FreqNode *br){
-        br->post->prev = br->prev;        
-        br->prev->post = br->post;
+    void del_slot(FreqNode *br){
+        if(br->post!=nullptr){
+            br->post->prev = br->prev;        
+        }
+        if(br->prev!=nullptr){
+            br->prev->post = br->post;
+        }
         delete br;
     }
 
@@ -115,29 +119,33 @@ struct LFUCache{
         if(it != cache.end()){
             Node *node = it->second;
             val = node->val;
-            node->slot->remove(node);
-            node->slot->append(node);
+            inc_freq(node);
             return 0;
         } else {
             return -1;
         }
+    }
+
+    void inc_freq(Node *node){
+        node->slot->remove(node);
+        int freq = node->slot->freq + 1;
+        if(node->slot->post == nullptr || node->slot->post->freq != freq){
+            add_slot(node->slot, freq);
+        }
+
+        if(node->slot->empty()){
+            del_slot(node->slot);
+        }
+
+        node->slot->post->append(node);
     }
     
     void put(const string &key, const string &val){
         auto it = cache.find(key);
         if(it != cache.end()){ //update
             Node *node = it->second;
-            node->slot->remove(node);
-            int freq = node->slot->freq + 1;
-            if(node->slot->post == nullptr || node->slot->post->freq != freq){
-                add_freq(node->slot, freq);
-            }
-
-            if(node->slot->empty()){
-                del_freq(node->slot);
-            }
-
-            node->slot->post->append(node);
+            node->val = val;
+            inc_freq(node);
             cache[key] = node;
         } else { //add
             if(size==capacity){
@@ -147,7 +155,7 @@ struct LFUCache{
             }
 
             if(freq_list.post==nullptr || freq_list.post->freq != 1){
-                add_freq(&freq_list, 1);
+                add_slot(&freq_list, 1);
             }
 
             Node *node = new Node(key, val);
--- lfu_cache/test_lfu.cpp
+++ lfu_cache/test_lfu.cpp
@@ -2,20 +2,41 @@
 #include <iostream>
 
 
+void display(LFUCache *lfu){
+    FreqNode *cur = lfu->freq_list.post;
+    while(cur!=nullptr){
+        cout<<"freq:"<<cur->freq<<"|";
+        Node *node = cur->first;
+        while(node!=nullptr){
+           cout<<"("<<node->key<<","<<node->val<<"),"; 
+           node = node->down;
+        }
+        cout<<endl;
+        cur = cur->post;
+    }
+}
+
 void test_lfu(){
     LFUCache lfu(3); 
     lfu.put("a", "123"); 
     lfu.put("b", "234"); 
     lfu.put("c", "555"); 
+    display(&lfu);
+
     lfu.put("a", "399"); 
-    string v;
-    lfu.get("b", v);
-    cout<<"v:"<<v<<endl;
+    string v1;
+    lfu.get("a", v1);
+    cout<<"val of a:"<<v1<<endl;
+    display(&lfu);
+
+    string v2;
+    lfu.get("b", v2);
+    cout<<"val of b:"<<v2<<endl;
+
+    display(&lfu);
 }
 
 int main(){
     test_lfu();
     return 0;
 }
-
-
