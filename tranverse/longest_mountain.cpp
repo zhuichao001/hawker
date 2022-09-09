a8c04f8cbe3c9423026e4e28d84fa9c330f77597
zhuichao001 <zhuichao001@163.com>
2022-09-09 11:15:27 +0800

    optimize implementaion for longeset mountain

--- tranverse/longest_mountain.cpp
+++ tranverse/longest_mountain.cpp
@@ -1,43 +1,30 @@
 #include <stdio.h>
 
-
 int longest_mountain(int *arr, int len){
     if(arr==nullptr || len<=2){
         return 0;
     }
-    int max = 0;
-    int cur = arr[1]>arr[0]?2:0;
-    int direction = arr[1]>arr[0]?1:-1;
-    for(int i=2; i<len; ++i){
-        if(direction==1){  //up
-            if(arr[i]<arr[i-1]){
-                if(cur>0){ //end first half
-                    ++cur;
-                    if(cur>max){
-                        max = cur;
-                    }
-                }
-                direction = -1;
-            }else{
-                ++cur;
-            }
-        }else{ //down
-            if(arr[i]<arr[i-1]){
-                if(cur>0){ //second half
-                    ++cur;
-                    if(cur>max){
-                        max = cur;
-                    }
-                }
-            }else{ //maybe start first half
-                direction = 1;
-                cur = 2;
-            }
-        } 
-    }
-    return max;
-}
 
+    int i = 1;
+    while(i<len && arr[i]<arr[i-1])++i;
+
+    int maxlen = 0;
+    int curlen = 1;
+    int direction = 1; //1:up, -1:down
+    for(; i<len; ++i){
+        if(arr[i]>arr[i-1]){ //in up hill
+            curlen = direction==1 ? curlen+1 : 2;
+            direction = 1;
+        }else{ //in down hill
+            ++curlen;
+            if(curlen>maxlen){
+                maxlen = curlen;
+            }
+            direction = -1;
+        }
+    }
+    return maxlen;
+}
 
 int main(){
     int a[] = {1,2,3,2,1};
@@ -48,4 +35,5 @@ int main(){
     fprintf(stderr, "b longest:%d\n", longest_mountain(b, sizeof(b)/sizeof(b[0])));
     fprintf(stderr, "c longest:%d\n", longest_mountain(c, sizeof(c)/sizeof(c[0])));
     fprintf(stderr, "d longest:%d\n", longest_mountain(d, sizeof(d)/sizeof(d[0])));
+    return 0;
 }
