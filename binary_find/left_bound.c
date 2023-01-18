4755fd3c641b2586faa0ed83e2a7690a26c88469
zhuichao001 <zhuichao001@163.com>
2023-01-18 20:37:00 +0800

    mod...

--- binary_find/left_bound.c
+++ binary_find/left_bound.c
@@ -1,8 +1,5 @@
 #include<iostream>
 
-using namespace std;
-
-
 int leftBound(int *nums, int len, int target) {
     if (nums==NULL || len == 0) return -1;
     int left = 0;
@@ -21,8 +18,7 @@ int leftBound(int *nums, int len, int target) {
     return left;
 }
 
-
 int main(){
     int a[]={1,2,3,3,3,3,5,6,8,9,10,12,18};
-    cout<<leftBound(a, sizeof(a)/sizeof(a[0]), 3)<<endl;
+    std::cout << leftBound(a, sizeof(a)/sizeof(a[0]), 3) << std::endl;
 }
