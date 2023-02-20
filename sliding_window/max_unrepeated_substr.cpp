54727bf15d7affe0094e8e96687884c9c4ee7650
zhuichao001 <zhuichao001@163.com>
2023-02-20 16:13:40 +0800

    modify

--- sliding_window/max_unrepeated_substr.cpp
+++ sliding_window/max_unrepeated_substr.cpp
@@ -8,7 +8,7 @@ int max_sublen(const std::string &src) {
     std::unordered_map<char, int> index;
     int res=0;
     for (int i=0, left=0; i<src.size(); ++i) {
-        const char &c = src[i];
+        const char c = src[i];
         if (index.find(c) == index.end()) {
             res = std::max(res, i-left+1);
         } else {
