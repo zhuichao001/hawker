bab9dca1bc2d26d7c87eda30770376d2cf28897d
zhuichao001 <zhuichao001@163.com>
2024-08-19 11:30:11 +0800

    modify Makefile

--- string/ip_to_uint.cpp
+++ string/ip_to_uint.cpp
@@ -5,34 +5,33 @@
 //return errcode. 0:success, -1:failed
 int ip_to_uint32(const std::string &ipstr, uint32_t &res){
     res = 0;
-    int len = ipstr.size();
 
     uint16_t sub = 0;
-    size_t dot_num = 0;
-    size_t dig_num = 0;
-    for (int i=0; i<len; ++i) {
+    size_t doc_cnt = 0;
+    size_t dig_cnt = 0;
+    for (int i=0; i<ipstr.size(); ++i) {
         if (ipstr[i]=='.') {
-            if (++dot_num==4 || dig_num==0) { //check format
+            if (++doc_cnt==4 || dig_cnt==0) { //check format
                 return -1;
             }
             res = (res<<8) + sub;
             sub = 0;
-            dig_num = 0;
+            dig_cnt = 0;
         } else if (ipstr[i]>='0' && ipstr[i]<='9') {
-            if (dig_num>0 && sub==0) { //check double-0
+            if (dig_cnt>0 && sub==0) { //check double-0
                 return -1;
             }
             sub = sub*10 + (ipstr[i]-'0');
-            if (sub>256) {
+            if (sub>=256) {
                 return -1;
             }
-            ++ dig_num;
+            ++dig_cnt;
         } else {
             return -1;
         }
     }
     res = (res<<8) + sub;
-    return dot_num==3 ? 0:-1;
+    return doc_cnt==3 ? 0:-1;
 }
 
 int main() {
