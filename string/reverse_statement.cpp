316a66ce96f86d62bd92fe2da843cb5e6d4795b2
zhuichao001 <zhuichao001@163.com>
2024-08-19 10:46:56 +0800

    翻转语句中单词顺序列

---
+++ string/reverse_statement.cpp
@@ -0,0 +1,35 @@
+#include<string>
+#include<string_view>
+#include<queue>
+#include<tuple>
+#include<iostream>
+
+
+void reverse_statement(const std::string &input, std::string &res){
+    std::deque<std::tuple<int,int>> stack;
+    int start=0,end=0;
+    for(;end<=input.size();++end){
+        if((input[end]==' '|| input[end]==0)&& input[start]!=' '){
+            stack.push_front(std::tuple<int,int>(start, end));
+            start=end;
+        }else if(input[start]==' ') {
+            start=end;
+        }
+    }
+
+    res.clear();
+    while(!stack.empty()){
+        auto [start, end] = stack.front();
+        stack.pop_front();
+        res += std::string_view(input.c_str()+start, end-start);
+        res += " ";
+    }
+}
+
+int main(){
+    std::string res;
+    std::string input="student. a am I";
+    reverse_statement(input, res);
+    std::cout << res << std::endl;
+    return 0;
+}
